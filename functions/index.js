81787480c16e2cdb3abe72d19c7cffea0249e9bf
Connor <s00172979@mail.itsligo.ie>
2019-02-24 15:19:37 +0000

    added function to send message when post is liked

--- functions/index.js
+++ functions/index.js
@@ -8,57 +8,81 @@ exports.addCreatedAtMessage = functions.firestore
   .document("messages/{messageId}")
   .onCreate((snap, context) => {
     const newcreatedAt = new Date();
-      
+
     return admin.firestore()
-            .collection("messages")
-            .doc(snap.id)
-            .set({
-              createdAt: newcreatedAt
-            }, {merge: true})
-            .catch(err => console.log(err))
+      .collection("messages")
+      .doc(snap.id)
+      .set({
+        createdAt: newcreatedAt
+      }, { merge: true })
+      .catch(err => console.log(err))
   });
 
-  //Adds a created time to created comment
-  exports.addCreatedAtComment = functions.firestore
+//Adds a created time to created comment
+exports.addCreatedAtComment = functions.firestore
   .document("comments/{commentId}")
   .onCreate((snap, context) => {
     const newcreatedAt = new Date();
-    
+
     return admin.firestore()
-            .collection("comments")
-            .doc(snap.id)
-            .set({
-              createdAt: newcreatedAt
-            }, {merge: true})
-            .catch(err => console.log(err))
+      .collection("comments")
+      .doc(snap.id)
+      .set({
+        createdAt: newcreatedAt
+      }, { merge: true })
+      .catch(err => console.log(err))
   });
 
-  //Adds a created time to created user
-  exports.addCreatedAtUser = functions.firestore
+//Adds a created time to created user
+exports.addCreatedAtUser = functions.firestore
   .document("users/{userId}")
   .onCreate((snap, context) => {
     const newcreatedAt = new Date();
 
     return admin.firestore()
-          .collection("users")
-          .doc(snap.id)
-          .set({
-            createdAt: newcreatedAt
-          }, {merge: true})
-          .catch(err => console.log(err))
+      .collection("users")
+      .doc(snap.id)
+      .set({
+        createdAt: newcreatedAt
+      }, { merge: true })
+      .catch(err => console.log(err))
   });
 
-  //Adds a craeted time to craeted post
-  exports.addCreatedAtPost = functions.firestore
+//Adds a craeted time to craeted post
+exports.addCreatedAtPost = functions.firestore
   .document("posts/{postId}")
   .onCreate((snap, context) => {
     const newcreatedAt = new Date();
 
     return admin.firestore()
-            .collection("posts")
-            .doc(snap.id)
-            .set({
-              createdAt: newcreatedAt
-            }, {merge: true})
-            .catch(err => console.log(err))
+      .collection("posts")
+      .doc(snap.id)
+      .set({
+        createdAt: newcreatedAt
+      }, { merge: true })
+      .catch(err => console.log(err))
+  });
+
+exports.onPostLike = functions.firestore.document("likes/{likeId}")
+  .onCreate((snapshot, context) => {
+        const data = snap.data();
+    console.log(data)
+    const post = admin.firestore().collection("posts").doc(data.postId).get();
+    const userId = post.UserID
+
+    const token = admin.firestore().collection("tokens").doc(userId).get();
+    const userToken = token.token
+
+    const liker = admin.firestore().collection("users").doc(data.userId).get();
+    const likerName = liker.displayName
+
+
+    const payload = {
+      notification: {
+        title: 'Jambox App',
+        body: likerName + ' liked your post'
+      }
+    };
+
+    admin.messaging().sendToDevice(userToken, payload)
   });
