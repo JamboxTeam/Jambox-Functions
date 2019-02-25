91fe631e39bb76fe11819f50615f8f6194acce67
s00172641 <s00172641@mail.itsligo.ie>
2019-02-25 15:15:00 +0000

    Notification for messages added

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
 admin.initializeApp();
 
@@ -9,13 +9,17 @@ exports.addCreatedAtMessage = functions.firestore
   .onCreate((snap, context) => {
     const newcreatedAt = new Date();
 
-    return admin.firestore()
+    return admin
+      .firestore()
       .collection("messages")
       .doc(snap.id)
-      .set({
-        createdAt: newcreatedAt
-      }, { merge: true })
-      .catch(err => console.log(err))
+      .set(
+        {
+          createdAt: newcreatedAt
+        },
+        { merge: true }
+      )
+      .catch(err => console.log(err));
   });
 
 //Adds a created time to created comment
@@ -24,13 +28,17 @@ exports.addCreatedAtComment = functions.firestore
   .onCreate((snap, context) => {
     const newcreatedAt = new Date();
 
-    return admin.firestore()
+    return admin
+      .firestore()
       .collection("comments")
       .doc(snap.id)
-      .set({
-        createdAt: newcreatedAt
-      }, { merge: true })
-      .catch(err => console.log(err))
+      .set(
+        {
+          createdAt: newcreatedAt
+        },
+        { merge: true }
+      )
+      .catch(err => console.log(err));
   });
 
 //Adds a created time to created user
@@ -39,13 +47,17 @@ exports.addCreatedAtUser = functions.firestore
   .onCreate((snap, context) => {
     const newcreatedAt = new Date();
 
-    return admin.firestore()
+    return admin
+      .firestore()
       .collection("users")
       .doc(snap.id)
-      .set({
-        createdAt: newcreatedAt
-      }, { merge: true })
-      .catch(err => console.log(err))
+      .set(
+        {
+          createdAt: newcreatedAt
+        },
+        { merge: true }
+      )
+      .catch(err => console.log(err));
   });
 
 //Adds a craeted time to craeted post
@@ -54,59 +66,134 @@ exports.addCreatedAtPost = functions.firestore
   .onCreate((snap, context) => {
     const newcreatedAt = new Date();
 
-    return admin.firestore()
+    return admin
+      .firestore()
       .collection("posts")
       .doc(snap.id)
-      .set({
-        createdAt: newcreatedAt
-      }, { merge: true })
-      .catch(err => console.log(err))
+      .set(
+        {
+          createdAt: newcreatedAt
+        },
+        { merge: true }
+      )
+      .catch(err => console.log(err));
   });
 
-exports.onPostLike = functions.firestore.document("likes/{likeId}")
+exports.onPostLike = functions.firestore
+  .document("likes/{likeId}")
   .onCreate((snap, context) => {
     const db = admin.firestore();
     const data = snap.data();
     var userRef = db.collection("users").doc(data.userId);
     var postRef = db.collection("posts").doc(data.postId);
-    var userToken = '';
+    var userToken = "";
 
-    return postRef.get().then(function(postdoc) {
-    //POST DOC
-      if (postdoc.exists) {
-        console.log("postdoc: ", postdoc.data().UserID)
-        db.collection("tokens").doc(postdoc.data().UserID).get().then(function(tokendoc) {
-        //TOKEN DOC
-            console.log("tokenDoc: ", tokendoc.data().token);
-            userToken = tokendoc.data().token;
-            userRef.get().then(function(userdoc) {
-            //USER DOC
+    return postRef
+      .get()
+      .then(function(postdoc) {
+        //POST DOC
+        if (postdoc.exists) {
+          console.log("postdoc: ", postdoc.data().UserID);
+          db.collection("tokens")
+            .doc(postdoc.data().UserID)
+            .get()
+            .then(function(tokendoc) {
+              //TOKEN DOC
+              console.log("tokenDoc: ", tokendoc.data().token);
+              userToken = tokendoc.data().token;
+              userRef.get().then(function(userdoc) {
+                //USER DOC
                 console.log("userDoc: ", userdoc);
                 let username = userdoc.data().displayName;
-                console.log("username: ", username)
+                console.log("username: ", username);
 
                 var message = {
                   data: {
-                    title: 'Jambox App',
+                    title: "Jambox App",
                     body: `${username} liked your post!`
                   },
                   token: userToken
                 };
-                console.log(message)
+                console.log(message);
 
-                admin.messaging().send(message)
-                  .then((respone) => {
-                    console.log('Successfully sent message:', respone);
+                admin
+                  .messaging()
+                  .send(message)
+                  .then(respone => {
+                    console.log("Successfully sent message:", respone);
                   })
-                  .catch((error) => {
-                    console.log('Error sending message', error)
+                  .catch(error => {
+                    console.log("Error sending message", error);
                   });
               });
-          });
-      } else {
+            });
+        } else {
           console.log("No such document!");
-      }
-    }).catch(function(error) {
+        }
+      })
+      .catch(function(error) {
         console.log("Error getting document:", error);
-    });
+      });
+  });
+
+exports.onMessageAdd = functions.firestore
+  .document("messages/{messageId}")
+  .onCreate((snap, context) => {
+    const db = admin.firestore();
+    const data = snap.data();
+    var privateChatsRef = db.collection("privatechats").doc(data.chatRoomID);
+    var senderRef = db.collection("users").doc(data.senderID);
+
+    return privateChatsRef
+      .get()
+      .then(function(privateChatDoc) {
+        //PRIVATE CHAT DOC
+        if (privateChatDoc.exists) {
+          console.log("privatechatdoc ", privateChatDoc.data());
+          privateChatDoc.data().members.forEach(member => {
+            if(member != data.senderID){
+            db.collection("tokens")
+              .doc(member)
+              .get()
+              .then(function(tokenDoc) {
+                //TOKEN DOC
+                console.log("tokenDoc", tokenDoc.data());
+                let userToken = tokenDoc.data().token;
+                senderRef.get().then(function(senderDoc) {
+                  //SENDER DOC
+                  console.log("senderDoc", senderDoc.data());
+                  let username = senderDoc.data().displayName;
+
+                  let time = new Date().setSeconds() - createdAt.seconds;
+
+                  var message = {
+                    data: {
+                      title: `${username}`,
+                      body: `${data.message} ${time}`
+                    },
+                    token: userToken
+                  };
+                  console.log(message);
+
+                  admin
+                    .messaging()
+                    .send(message)
+                    .then(respone => {
+                      console.log("Successfully sent message:", respone);
+                    })
+                    .catch(error => {
+                      console.log("Error sending message", error);
+                    });
+                });
+              });
+            }
+              //end loop
+          });
+        } else {
+          console.log("No such document");
+        }
+      })
+      .catch(function(error) {
+        console.log("Error getting document: ", error);
+      });
   });
